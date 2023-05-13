6f4ca6610cc8adfbb1e60e4f548f54935d5d44c5
Thompson Edolo <907685+verygreenboi@users.noreply.github.com>
2023-05-13 18:41:31 +0100

    Feature/nav menu (#13)
    * chore: cleanup component
    
    * feat: add links and fixes

--- src/header/header/header.component.ts
+++ src/header/header/header.component.ts
@@ -1,46 +1,16 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component, ElementRef,
-  HostBinding,
-  HostListener,
-  OnDestroy,
-  OnInit
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { HamburgerComponent } from '../hamburger/hamburger.component';
 import { NavMenuComponent } from '../nav-menu/nav-menu.component';
-import { BehaviorSubject, distinctUntilChanged, map, startWith, Subject, takeUntil, tap } from 'rxjs';
-import { BgService } from '../../core/services';
+import { CloseMenuDirective } from '../close-menu.directive';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'header[opy-header]',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, HamburgerComponent, NavMenuComponent],
+  imports: [CommonModule, NgOptimizedImage, HamburgerComponent, NavMenuComponent, CloseMenuDirective, RouterLink],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent {
-  destroy$ = new Subject<void>();
-  collapse = new BehaviorSubject<boolean>(false);
-  isCollapsed$ = this.collapse.asObservable().pipe(
-    startWith(false),
-    map(collapse => !collapse),
-    takeUntil(this.destroy$)
-  );
-
-  headerBgClass$ = this.bgService.headerDarkBg$.pipe(
-    map(darkBg => darkBg ? 'dark-bg' : ''),
-    startWith(''),
-    distinctUntilChanged(),
-    takeUntil(this.destroy$)
-  );
-
-
-  constructor(
-    private el: ElementRef,
-    private bgService: BgService,
-  ) {}
-
-}
+export class HeaderComponent {}
